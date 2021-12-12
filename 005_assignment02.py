aa5b5316eb11c45fa07eea0fb1c4b3b111ff777e
vermapawan <95943199+vermapawan@users.noreply.github.com>
2021-12-12 15:08:58 -0500

    Add files via upload
    done

---
+++ 005_assignment02.py
@@ -0,0 +1,9 @@
+x=input("input the side a length: ")
+a_side=float(x)
+y=input("input the side b length: ")
+b_side=float(y)
+c=((a_side**2+b_side**2)**0.5)
+print("the ans is ",c)
+
+            
+                
